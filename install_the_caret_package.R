878bd8a35d5620c17411a28d5bba59cde81a6771
Joseph P. Yurko <jyurko@gmail.com>
2020-08-16 23:49:41 -0400

    initial commit

---
+++ install_the_caret_package.R
@@ -0,0 +1,12 @@
+### we already installed `tidyverse` with the RStudio GUI
+
+### we will now install the `caret` package using the `install.packages()`
+### function.
+
+### note when I have backticks `` that corresponds to a variable
+### or function name.
+
+### check the already installed packages
+# installed.packages()
+
+install.packages("caret")
---
+++ my_first_r_script.R
@@ -0,0 +1,14 @@
+# this is a comment
+
+### here's another comment
+
+3 + 3
+
+8 + 2
+
+### assign a variable
+x = 1
+
+y <- 1
+
+### 
